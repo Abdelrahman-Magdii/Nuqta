6813a580a8f18df42651b821f628a58d6c96c5d4
Abdelrahman-Magdii <abdulrahman403828@fci.bu.edu>
2025-04-16 17:05:41 +0200

    prod

--- src/main/java/com/spring/nuqta/config/RedisConfig.java
+++ src/main/java/com/spring/nuqta/config/RedisConfig.java
@@ -1,35 +1,36 @@
 package com.spring.nuqta.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-
-import java.net.URI;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
 @Profile("prod")
 public class RedisConfig {
 
-    @Value("${REDIS_URL}")
-    private String redisUrl;
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        URI redisUri = URI.create(redisUrl);
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-
-        config.setHostName(redisUri.getHost());
-        config.setPort(redisUri.getPort());
-        String userInfo = redisUri.getUserInfo();
-        if (userInfo != null && userInfo.contains(":")) {
-            config.setPassword(RedisPassword.of(userInfo.split(":", 2)[1]));
-        }
-
-        return new LettuceConnectionFactory(config);
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
     }
 }
 
