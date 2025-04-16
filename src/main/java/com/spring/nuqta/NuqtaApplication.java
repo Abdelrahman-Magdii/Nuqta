d74f93b77ad892cee7afd6a8b719c496d5df8e64
Abdelrahman-Magdii <abdulrahman403828@fci.bu.edu>
2025-04-16 17:37:15 +0200

    prod

--- src/main/java/com/spring/nuqta/NuqtaApplication.java
+++ src/main/java/com/spring/nuqta/NuqtaApplication.java
@@ -2,6 +2,7 @@ package com.spring.nuqta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaAuditing
 @EnableCaching
 @EnableJpaRepositories(basePackages = "com.spring.nuqta")
+@EntityScan(basePackages = "com.spring.nuqta")
 public class NuqtaApplication {
 
     public static void main(String[] args) {
