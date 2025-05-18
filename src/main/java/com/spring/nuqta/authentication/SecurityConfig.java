521a910c8f9b53f1df909c81f714e7583c74cb0e
Abdelrahman-Magdii <abdulrahman403828@fci.bu.edu>
2025-05-18 18:53:16 +0300

    CICD Pipline

--- src/main/java/com/spring/nuqta/authentication/SecurityConfig.java
+++ src/main/java/com/spring/nuqta/authentication/SecurityConfig.java
@@ -33,7 +33,7 @@ import static com.spring.nuqta.enums.Scope.USER;
 public class SecurityConfig {
 
     public static final String[] PUBLIC_APIS = {"/swagger-ui/**", "/api/auth/**",
-            "/api-docs/**", "/", "/verify", "/verification-success.html", "/verification-failed.html"};
+            "/api-docs/**", "/", "/verify", "/verification-success.html", "/verification-failed.html", "/static/styles.css"};
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final MessageSource ms;
